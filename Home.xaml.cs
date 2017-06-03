b0bc2d617b7f40b07975e5a23bf845cfef0d7200
Dekei <nathancollard1@gmail.com>
2017-06-03 16:22:20 +0200

    Fonctionnalités avancées
    Panels détaillés fonctionnels

--- Home.xaml.cs
+++ Home.xaml.cs
@@ -81,19 +81,19 @@ namespace Panel_EchoesWar
                 case "botadminPlayButton":
                     statusText.Content = prefix + "Dernière commande exécutée: Démarrage du Bot Admin";
                     WebClient c6 = new WebClient();
-                    c6.OpenRead("http://panel.echoeswar.fr/pages/tsscript.php?command=reboot");
+                    c6.OpenRead("http://panel.echoeswar.fr/pages/botscript.php?command=start");
                     c6.Dispose();
                     break;
                 case "botadminStopButton":
                     statusText.Content = prefix + "Dernière commande exécutée: Arrêt du Bot Admin";
                     WebClient c7 = new WebClient();
-                    c7.OpenRead("http://panel.echoeswar.fr/pages/tsscript.php?command=reboot");
+                    c7.OpenRead("http://panel.echoeswar.fr/pages/botscript.php?command=stop");
                     c7.Dispose();
                     break;
                 case "botadminRestartButton":
                     statusText.Content = prefix + "Dernière commande exécutée: Redémarrage du Bot Admin";
                     WebClient c8 = new WebClient();
-                    c8.OpenRead("http://panel.echoeswar.fr/pages/tsscript.php?command=reboot");
+                    c8.OpenRead("http://panel.echoeswar.fr/pages/botscript.php?command=reboot");
                     c8.Dispose();
                     break;
                 case "botradioPlayButton":
@@ -126,24 +126,123 @@ namespace Panel_EchoesWar
             }
         }
 
+        private void Command_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = (Button)e.Source;
+            String prefix = "";
+            WebClient w = new WebClient();
+            switch (b.Name)
+            {
+                case "CommandTeamSpeakPlayButton":
+                    statusText.Content = prefix + "Dernière commande exécutée: Démarrage du serveur TeamSpeak";
+                    w.OpenRead("http://panel.echoeswar.fr/pages/tsscript.php?command=start");
+                    w.Dispose();
+                    TeamSpeakStatus.Content = "Status : " + getDataReturn("http://panel.echoeswar.fr/pages/status.php?server=ts");
+                    break;
+                case "CommandTeamSpeakStopButton":
+                    statusText.Content = prefix + "Dernière commande exécutée: Arrêt du serveur TeamSpeak";
+                    w.OpenRead("http://panel.echoeswar.fr/pages/tsscript.php?command=stop");
+                    w.Dispose();
+                    TeamSpeakStatus.Content = "Status : " + getDataReturn("http://panel.echoeswar.fr/pages/status.php?server=ts");
+                    break;
+                case "CommandTeamSpeakRestartButton":
+                    statusText.Content = prefix + "Dernière commande exécutée: Redémarrage du serveur TeamSpeak";
+                    w.OpenRead("http://panel.echoeswar.fr/pages/tsscript.php?command=reboot");
+                    w.Dispose();
+                    TeamSpeakStatus.Content = "Status : " + getDataReturn("http://panel.echoeswar.fr/pages/status.php?server=ts");
+                    break;
+                case "CommandBotAdminPlayButton":
+                    statusText.Content = prefix + "Dernière commande exécutée: Démarrage du serveur Bot Admin";
+                    w.OpenRead("http://panel.echoeswar.fr/pages/botscript.php?command=start");
+                    w.Dispose();
+                    BotAdminStatus.Content = "Status : " + getDataReturn("http://panel.echoeswar.fr/pages/status.php?server=botadmin");
+                    break;
+                case "CommandBotAdminStopButton":
+                    statusText.Content = prefix + "Dernière commande exécutée: Arrêt du serveur Bot Admin";
+                    w.OpenRead("http://panel.echoeswar.fr/pages/botscript.php?command=stop");
+                    w.Dispose();
+                    BotAdminStatus.Content = "Status : " + getDataReturn("http://panel.echoeswar.fr/pages/status.php?server=botadmin");
+                    break;
+                case "CommandBotAdminRestartButton":
+                    statusText.Content = prefix + "Dernière commande exécutée: Redémarrage du serveur Bot Admin";
+                    w.OpenRead("http://panel.echoeswar.fr/pages/botscript.php?command=reboot");
+                    w.Dispose();
+                    BotAdminStatus.Content = "Status : " + getDataReturn("http://panel.echoeswar.fr/pages/status.php?server=botadmin");
+                    break;
+                case "CommandBotRadioPlayButton":
+                    statusText.Content = prefix + "Dernière commande exécutée: Démarrage du serveur Bot Radio";
+                    w.OpenRead("http://panel.echoeswar.fr/pages/radioscript.php?command=start");
+                    w.Dispose();
+                    BotRadioStatus.Content = "Status : " + getDataReturn("http://panel.echoeswar.fr/pages/status.php?server=botradio");
+                    break;
+                case "CommandBotRadioStopButton":
+                    statusText.Content = prefix + "Dernière commande exécutée: Arrêt du serveur Bot Radio";
+                    w.OpenRead("http://panel.echoeswar.fr/pages/radioscript.php?command=stop");
+                    w.Dispose();
+                    BotRadioStatus.Content = "Status : " + getDataReturn("http://panel.echoeswar.fr/pages/status.php?server=botradio");
+                    break;
+                case "CommandBotRadioRestartButton":
+                    statusText.Content = prefix + "Dernière commande exécutée: Redémarrage du serveur Bot Radio";
+                    w.OpenRead("http://panel.echoeswar.fr/pages/radioscript.php?command=reboot");
+                    w.Dispose();
+                    BotRadioStatus.Content = "Status : " + getDataReturn("http://panel.echoeswar.fr/pages/status.php?server=botradio");
+                    break;
+                case "CommandSQLPlayButton":
+                    statusText.Content = prefix + "Dernière commande exécutée: Démarrage du serveur SQL";
+                    w.OpenRead("http://panel.echoeswar.fr/pages/mysqlscript.php?command=start");
+                    w.Dispose();
+                    WebStatus.Content = "Status : " + getDataReturn("http://panel.echoeswar.fr/pages/status.php?server=web");
+                    SQLStatus.Content = "Status : " + getDataReturn("http://panel.echoeswar.fr/pages/status.php?server=web");
+                    break;
+                case "CommandSQLStopButton":
+                    statusText.Content = prefix + "Dernière commande exécutée: Arrêt du serveur SQL";
+                    w.OpenRead("http://panel.echoeswar.fr/pages/mysqlscript.php?command=stop");
+                    w.Dispose();
+                    WebStatus.Content = "Status : " + getDataReturn("http://panel.echoeswar.fr/pages/status.php?server=web");
+                    SQLStatus.Content = "Status : " + getDataReturn("http://panel.echoeswar.fr/pages/status.php?server=web");
+                    break;
+                case "CommandSQLRebootButton":
+                    statusText.Content = prefix + "Dernière commande exécutée: Redémarrage du serveur SQL";
+                    w.OpenRead("http://panel.echoeswar.fr/pages/mysqlscript.php?command=reboot");
+                    w.Dispose();
+                    WebStatus.Content = "Status : " + getDataReturn("http://panel.echoeswar.fr/pages/status.php?server=web");
+                    SQLStatus.Content = "Status : " + getDataReturn("http://panel.echoeswar.fr/pages/status.php?server=web");
+                    break;
+            }
+        }
+
         private void TeamSpeakBlock_MouseDown(object sender, MouseButtonEventArgs e)
         {
             showContainer(BlockContainer, TeamSpeakContainer);
+            TeamSpeakStatus.Content = "Status : " + getDataReturn("http://panel.echoeswar.fr/pages/status.php?server=ts");
         }
 
         private void BotAdminBlock_MouseDown(object sender, MouseButtonEventArgs e)
         {
             showContainer(BlockContainer, BotAdminContainer);
+            BotAdminStatus.Content = "Status : " + getDataReturn("http://panel.echoeswar.fr/pages/status.php?server=botadmin");
         }
 
         private void BotRadioBlock_MouseDown(object sender, MouseButtonEventArgs e)
         {
             showContainer(BlockContainer, BotRadioContainer);
+            BotRadioStatus.Content = "Status : " + getDataReturn("http://panel.echoeswar.fr/pages/status.php?server=botradio");
         }
 
         private void WebBlock_MouseDown(object sender, MouseButtonEventArgs e)
         {
             showContainer(BlockContainer, WebContainer);
+            WebStatus.Content = "Status : " + getDataReturn("http://panel.echoeswar.fr/pages/status.php?server=web");
+            SQLStatus.Content = "Status : " + getDataReturn("http://panel.echoeswar.fr/pages/status.php?server=web");
+        }
+
+        public string getDataReturn(string url)
+        {
+            WebClient client = new WebClient();
+            string html = client.DownloadString(url);
+            string status = html.Substring(html.IndexOf("<data>") + ("<data>").Length);
+            status = status.Substring(0, status.IndexOf("</data>"));
+            return status;
         }
 
         public void showContainer(Grid s, Grid d)
